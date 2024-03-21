7c13a62e5d182d9c499de42e73df773c52f214ab
vaughankraska <vaughankraska@gmail.com>
2024-03-21 11:44:36 +0100

    finish stupidly long solution to pt 2 with enums

--- projects/advent_of_code/2023/day_one_trebuchet/src/main.rs
+++ projects/advent_of_code/2023/day_one_trebuchet/src/main.rs
@@ -1,3 +1,4 @@
+use std::fs;
 use std::fs::File;
 use std::io::{self, BufRead, BufReader, Lines};
 use std::num::ParseIntError;
@@ -8,11 +9,9 @@ fn main() {
     if let Ok(lines) = read_lines("./input.txt") {
         for line_result in lines {
             let line = line_result.unwrap();
-            println!();
             let mut num_vector: Vec<char> = Vec::new();
             for character in line.chars() {
                 if let Some(num) = character.to_digit(10) {
-                    print!("{}", num);
                     num_vector.push(character);
                 }
             }
@@ -22,8 +21,9 @@ fn main() {
             };
         }
     }
-    part_two();
+    let pt_2_answer = part_two();
     println!("pt 1: ANSWER: {}", answer);
+    println!("pt 2: ANSWER: {}", pt_2_answer);
 }
 
 fn get_to_add(char_vector: Vec<char>) -> Result<u32, ParseIntError> {
@@ -38,17 +38,13 @@ fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>> wher
 
 // Do part two using more than just brute force
 // goals:
-// use struct and implement methods on that struct
+// use enum and implement methods on that enum
 // repeat all methods for the sake of repetition: "Repetition är kunskapens moder"
 // only reference the docs and previous written files
 
-#[derive(Debug)]
-struct Calibration {
-    raw_lines: Vec<String>,
-    parsed_lines: Vec<Vec<u32>>,
-    calibration_values: Vec<u32>,
-}
-enum WrittenOutNumber {
+
+#[derive(Debug, PartialEq)]
+enum Digit {
     One,
     Two,
     Three,
@@ -59,118 +55,145 @@ enum WrittenOutNumber {
     Eight,
     Nine,
 }
-impl WrittenOutNumber {
+
+struct DigitIterator {
+    digit: Digit,
+    index: u8,
+}
+impl Iterator for DigitIterator {
+    type Item = Digit;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.index += 1;
+        Digit::from_int(self.index)
+    }
+}
+
+impl Digit {
     fn to_string(&self) -> String {
         match self {
-            WrittenOutNumber::One => String::from("one"),
-            WrittenOutNumber::Two => String::from("two"),
-            WrittenOutNumber::Three => String::from("three"),
-            WrittenOutNumber::Four => String::from("four"),
-            WrittenOutNumber::Five => String::from("five"),
-            WrittenOutNumber::Six => String::from("six"),
-            WrittenOutNumber::Seven => String::from("seven"),
-            WrittenOutNumber::Eight => String::from("eight"),
-            WrittenOutNumber::Nine => String::from("nine"),
+            Digit::One => String::from("one"),
+            Digit::Two => String::from("two"),
+            Digit::Three => String::from("three"),
+            Digit::Four => String::from("four"),
+            Digit::Five => String::from("five"),
+            Digit::Six => String::from("six"),
+            Digit::Seven => String::from("seven"),
+            Digit::Eight => String::from("eight"),
+            Digit::Nine => String::from("nine"),
         }
     }
-    fn match_from_string(input: String) -> WrittenOutNumber {
-        match input.to_lowercase() {
-            String::from("one") => WrittenOutNumber::One,
-            String::from("two") => WrittenOutNumber::Two,
-            String::from("three") => WrittenOutNumber::Three,
-            String::from("four") => WrittenOutNumber::Four,
-            String::from("five") => WrittenOutNumber::Five,
-            String::from("six") => WrittenOutNumber::Six,
-            String::from("seven") => WrittenOutNumber::Seven,
-            String::from("eight") => WrittenOutNumber::Eight,
-            String::from("nine") => WrittenOutNumber::Nine,
+    fn to_int(&self) -> u8 {
+        match self {
+            Digit::One => 1,
+            Digit::Two => 2,
+            Digit::Three => 3,
+            Digit::Four => 4,
+            Digit::Five => 5,
+            Digit::Six => 6,
+            Digit::Seven => 7,
+            Digit::Eight => 8,
+            Digit::Nine => 9,
         }
     }
 
-    fn to_int(&self) {
-
-    }
-}
-impl Calibration {
-    fn parse(input: Lines<BufReader<File>>) -> Option<Calibration> {
-        if let lines = input.flatten() {
-            // let raw_lines: Vec<String> = lines.collect::<Vec<String>>();
-            let mut raw_lines: Vec<String> = Vec::new();
-            let mut parsed_lines: Vec<Vec<u32>> = Vec::new();
-            let mut calibration_values: Vec<u32> = Vec::new();
-            for line in lines {
-                raw_lines.push(line);
-                parsed_lines.push(line);
-            }
-            Some(Calibration{
-                raw_lines,
-                parsed_lines,
-                calibration_values,
-            })
-        } else {
-            None
+    fn from_string(input: &str) -> Option<Digit> {
+        match input.to_lowercase().as_str() {
+            "one" => Some(Digit::One),
+            "two" => Some(Digit::Two),
+            "three" => Some(Digit::Three),
+            "four" => Some(Digit::Four),
+            "five" => Some(Digit::Five),
+            "six" => Some(Digit::Six),
+            "seven" => Some(Digit::Seven),
+            "eight" => Some(Digit::Eight),
+            "nine" => Some(Digit::Nine),
+            _ => None,
         }
     }
-}
-// struct CalibrationDocument {
-//     reader: BufReader<File>,
-//     calibration_value: CalibrationValue,
-// }
 
-fn read_lines_again<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>> where P: AsRef<Path>, {
-    let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
-}
-fn part_two(){
-    let cal_test = Calibration::parse(read_lines_again("input_small_pt2.txt").unwrap());
-    println!("cal test {:?}", cal_test);
+    fn from_int(integer: u8) -> Option<Digit> {
+        match integer {
+            1 => Some(Digit::One),
+            2 => Some(Digit::Two),
+            3 => Some(Digit::Three),
+            4 => Some(Digit::Four),
+            5 => Some(Digit::Five),
+            6 => Some(Digit::Six),
+            7 => Some(Digit::Seven),
+            8 => Some(Digit::Eight),
+            9 => Some(Digit::Nine),
+            _ => None,
+        }
+    }
+
+    fn iter() -> DigitIterator {
+        DigitIterator {
+            digit: Digit::One,
+            index: 0,
+        }
+    }
+
+    fn parse_occurrences(input: &str) -> Vec<Digit> {
+        let mut ocurrences: Vec<(usize, Digit)> = Vec::new();
+        let iter = Digit::iter();
+        let to_check = input.to_lowercase();
+        for digit in iter {
+            // check written out "one", "nine", ...
+            to_check
+                .match_indices(digit.to_string().as_str())
+                .for_each(|(bindex, value)| {
+                    let idx = ocurrences.partition_point(|(at, val)| at < &bindex);
+                    ocurrences.insert(idx, (bindex, Digit::from_int(digit.to_int()).unwrap()));
+                });
+        };
+        // check string digits eg. "1", "9", ...
+        to_check.match_indices(char::is_numeric)
+            .for_each(|(bindex, value)| {
+                let idx = ocurrences.partition_point(|(at, val)| at < &bindex);
+                ocurrences.insert(idx, (bindex, Digit::from_int(value.parse().unwrap()).unwrap()));
+            });
+
+        ocurrences.iter()
+            .map(|(_, value)| Digit::from_int(value.to_int()).unwrap())
+            .collect()
+    }
 }
 
-// fn main() {
-//     let mut parsed_document: Vec<(String, String)> = Vec::new();
-//     if let Ok(lines) = read_lines("input.txt") {
-//         for line in lines.flatten() {
-//             let (first_half, second_half) = line.split_at(line.len().div_ceil(2));
-//             parsed_document.push((first_half.to_string(), second_half.chars().rev().collect::<String>()))
-//         }
-//     }
-//     let mut total = 0;
-//     for item in parsed_document {
-//
-//         let mut dig1: Option<char> = None;
-//         let mut dig2: Option<char> = None;
-//
-//         for (index, a_char) in item.0.chars().enumerate() {
-//
-//             let other_char = item.1.chars().nth(index).unwrap_or('-');
-//
-//             if dig1.is_some() && dig2.is_some() {
-//                 println!("Found two digits early: {}{}", dig1.unwrap(), dig2.unwrap());
-//                 break;
-//             }
-//             if a_char.to_digit(10).is_some() && dig1.is_none(){
-//                 dig1 = Some(a_char);
-//             }
-//             if other_char.to_digit(10).is_some() && dig2.is_none() {
-//                 dig2 = Some(other_char);
-//             }
-//
-//         }
-//         if dig1.is_some() && dig2.is_some() {
-//             let concatenated = format!("{}{}", dig1.unwrap(), dig2.unwrap());
-//             match concatenated.parse::<i32>() {
-//                 Ok(parsed_number) => total += parsed_number,
-//                 Err(_) => eprintln!("Could not parse digit pair into number"),
-//             }
-//         }
-//         if dig1.is_some() && dig2.is_none() {
-//             let concatenated = format!("{}{}", dig1.unwrap(), dig1.unwrap());
-//             match concatenated.parse::<i32>() {
-//                 Ok(parsed_number) => total += parsed_number,
-//                 Err(_) => eprintln!("Could not parse first digit as itself twice"),
-//             }
-//         }
-//     }
-//
-//     println!("Final code is {total}");
-// }
+fn concat_digits(first: &Digit, second: &Digit) -> u32 {
+    format!("{}{}", first.to_int(), second.to_int()).parse().unwrap()
+}
+
+#[test]
+fn test_digit() {
+
+    assert_eq!(Digit::from_string("one").unwrap().to_int(), 1);
+
+    assert_eq!(Digit::Five.to_int(), 5);
+
+    assert_eq!(Digit::Six.to_string(), "six");
+
+    assert_eq!(Digit::parse_occurrences("onetwothreefour"),
+               [Digit::One, Digit::Two, Digit::Three, Digit::Four]);
+    assert_eq!(Digit::parse_occurrences("eightwothree"),
+               [Digit::Eight, Digit::Two, Digit::Three]);
+    assert_eq!(Digit::parse_occurrences("1f2"),
+               [Digit::One, Digit::Two]);
+    assert_eq!(Digit::parse_occurrences("one2three4"),
+               [Digit::One, Digit::Two, Digit::Three, Digit::Four]);
+}
+
+
+fn read_lines_simpler(filename: &str) -> String {
+    fs::read_to_string(filename).expect("Failed to read file to string")
+}
+fn part_two() -> u32 {
+    let input = read_lines_simpler("input_pt2.txt");
+    let mut total = 0;
+    for line in input.lines() {
+        // println!("{},{:?}", line, Digit::parse_occurrences(line));
+        let parsed = Digit::parse_occurrences(line);
+        total += concat_digits(parsed.first().unwrap(), parsed.last().unwrap());
+    }
+    total
+}
