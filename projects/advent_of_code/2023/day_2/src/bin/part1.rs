a02baf9fd9c079b80db5068a52aea710919f2cc2
vaughankraska <vaughankraska@gmail.com>
2024-03-29 22:30:00 +0100

    =in right format now

--- projects/advent_of_code/2023/day_2/src/bin/part1.rs
+++ projects/advent_of_code/2023/day_2/src/bin/part1.rs
@@ -4,49 +4,53 @@ const RED_THRESH: u8 = 12;
 const GREEN_THRESH: u8 = 13;
 const BLUE_THRESH: u8 = 14;
 
-#[derive(Debug)]
-enum Color {
-    Red,
-    Green,
-    Blue,
-}
+pub mod day_2 {
 
-impl Color {
-    fn from_string(input: &str) -> Option<Color> {
-        assert!(input.contains("green") || input.contains("blue") || input.contains("red"));
-        match input {
-            "green" => Some(Color::Green),
-            "blue" => Some(Color::Blue),
-            "red" => Some(Color::Red),
-            _ => None
-        }
+    #[derive(Debug)]
+    pub enum Color {
+        Red,
+        Green,
+        Blue,
     }
-}
-#[derive(Debug)]
-struct CubePull {
-    color: Color,
-    count: u32,
-}
 
-impl CubePull {
-    fn from_string(input: &str) -> CubePull {
-        let (count, color) = input.trim().split_once(" ")
-            .expect("Did not match format '<number> <color>'");
-        CubePull {
-            color: Color::from_string(color).unwrap(),
-            count: count.parse::<u32>().expect("Cube count could not be parsed"),
+    impl Color {
+        pub fn from_string(input: &str) -> Option<Color> {
+            assert!(input.contains("green") || input.contains("blue") || input.contains("red"));
+            match input {
+                "green" => Some(Color::Green),
+                "blue" => Some(Color::Blue),
+                "red" => Some(Color::Red),
+                _ => None
+            }
         }
     }
 
-    fn is_possible(&self) -> bool {
-        match self.color {
-            Color::Blue => self.count <= BLUE_THRESH as u32,
-            Color::Red => self.count <= RED_THRESH as u32,
-            Color::Green => self.count <= GREEN_THRESH as u32,
+    #[derive(Debug)]
+    pub struct CubePull {
+        pub color: Color,
+        pub count: u32,
+    }
+
+    impl CubePull {
+        pub fn from_string(input: &str) -> CubePull {
+            let (count, color) = input.trim().split_once(" ")
+                .expect("Did not match format '<number> <color>'");
+            CubePull {
+                color: Color::from_string(color).unwrap(),
+                count: count.parse::<u32>().expect("Cube count could not be parsed"),
+            }
+        }
+
+        pub fn is_possible(&self) -> bool {
+            match self.color {
+                Color::Blue => self.count <= super::BLUE_THRESH as u32,
+                Color::Red => self.count <= super::RED_THRESH as u32,
+                Color::Green => self.count <= super::GREEN_THRESH as u32,
+            }
         }
     }
-}
 
+}
 fn main() {
     println!("part one");
     let real_input = fs::read_to_string("src/input_pt1.txt")
@@ -56,17 +60,17 @@ fn main() {
 }
 
 fn part_1(input: &str) -> u32 {
-    let result: Vec<(u32, Vec<Vec<CubePull>>)> = input.lines()
+    let result: Vec<(u32, Vec<Vec<day_2::CubePull>>)> = input.lines()
         .map(|game| {
             let (game_str, rounds_string) = game.split_once(":").unwrap();
 
             let game_number = game_str.split_once(" ").unwrap().1
                 .parse::<u32>().unwrap();
 
-            let rounds: Vec<Vec<CubePull>> = rounds_string.split(";")
+            let rounds: Vec<Vec<day_2::CubePull>> = rounds_string.split(";")
                 .map(|round| {
                     round.trim().split(",")
-                        .map(|pull| CubePull::from_string(pull))
+                        .map(|pull| day_2::CubePull::from_string(pull))
                         .collect()
                 }).collect();
             (game_number, rounds)
--- projects/advent_of_code/2023/day_2/src/bin/part2.rs
+++ projects/advent_of_code/2023/day_2/src/bin/part2.rs
@@ -1,3 +1,70 @@
+mod part1;
+
+use std::fs;
+
+use crate::part1::day_2::*;
+
 fn main() {
     println!("part 2");
+
+    let input = fs::read_to_string("src/input_pt1.txt").expect("could not find file or something");
+    let answer = part_2(&input);
+    println!("ANSWER PT_2: {}", answer);
+
+}
+
+fn part_2(input: &str) -> u32 {
+    let answer = input.lines()
+        .map(|game| {
+            let (game_str, rounds_str) = game.split_once(":").unwrap();
+            let game_id = game_str.split_once(" ").unwrap().1
+                .parse::<u32>().unwrap();
+
+            let mut color_map: (CubePull, CubePull, CubePull) = (
+                CubePull{ color: Color::Red, count: 0},
+                CubePull{ color: Color::Green, count: 0},
+                CubePull{ color: Color::Blue, count: 0}
+                );
+
+            let rounds: Vec<CubePull> = rounds_str.split(";")
+                .map(|round| {
+                    round.trim().split(",")
+                        .map(|pull|CubePull::from_string(pull))
+                        .collect::<Vec<CubePull>>()
+                }).flatten()
+            .collect();
+
+            for pull in rounds {
+                match pull.color {
+                    Color::Red => {
+                        if color_map.0.count < pull.count { color_map.0.count = pull.count};
+                    },
+                    Color::Green => {
+                        if color_map.1.count < pull.count {color_map.1.count = pull.count};
+                    },
+                    Color::Blue => {
+                        if color_map.2.count < pull.count {color_map.2.count = pull.count};
+                    },
+                };
+            }
+
+            (game_id, color_map)
+        })
+    .collect::<Vec<(u32, (CubePull, CubePull, CubePull))>>();
+    dbg!(answer);
+    100_000
+}
+
+#[cfg(test)]
+mod part_2_tests {
+    use std::fs;
+    use super::*;
+
+    #[test]
+    fn small_answer_works() {
+        // uses same file as part 1
+        let input_small = fs::read_to_string("src/input_pt1_sm.txt")
+            .expect("problem with file");
+        assert_eq!(part_2(&input_small), 2286);
+    }
 }
